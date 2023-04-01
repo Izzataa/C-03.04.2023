42a2904c24b775331a07e0b17f288e9987a4bca3
izzataa <izzataa@code.edu.az>
2023-04-01 19:49:35 +0400

    finished

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+//task 1 start
+//int Num = 70;
+//if (Num < 0 || Num > 100 )
+//{
+//    Console.WriteLine("Deyeri duzgun daxil et");
+//}
+//else if (Num < 100 && Num > 90)
+//{
+//    Console.WriteLine("A");
+//}
+//else if(Num > 80 && Num < 91){
+//    Console.WriteLine("B");
+
+//} else if ( Num > 70 && Num < 81)
+//{
+//    Console.WriteLine("C");
+//}
+//else if (Num > 60 && Num < 71)
+//{
+//    Console.WriteLine("D");
+//}else  if (Num >50  && Num < 61)
+//{
+//    Console.WriteLine("E");
+//}else
+//{
+//    Console.WriteLine("Kesildiniz") ;
+//}
+//task 1 end
+//task 2 start
+//int[] nums = { -1, 5, 7, -13, -77, 15 };
+//int countplus = 0;
+//int countminus = 0;
+//for (int i = 0; i < nums.length; i++)
+//{
+//    if (nums[i] > 0)
+//    {
+//        countplus++;
+//    }
+//    else if (nums[i] < 0)
+//    {
+//        countminus++;
+//    }
+//}
+//console.writeline("menfi ededler");
+//console.writeline(countminus);
+//console.writeline("musbet ededler");
+//console.writeline(countplus);
+// task 2 end
+//
+// task 3 start 
+
+//int[] Nums = { 22, 14, 55, 27, 35 };
+//int max_Num=0;
+
+//for (int i = 0; i < Nums.Length; i++)
+//{
+//    if (Nums[i] > max_Num)
+//    {
+//        max_Num = Nums[i];
+//    }
+//}
+//Console.WriteLine(max_Num);
+// task 3 end
